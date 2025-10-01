d0c08c9563bd055de9a5265215aa5ab3b15a0253
Connor <connor.tollett@icloud.com>
2025-09-30 22:29:21 -0400

    modified tripadvisor

--- js/tripadvisor.js
+++ js/tripadvisor.js
@@ -1,7 +1,7 @@
 function handleTripAdvisor(value, os) {
   const name = "TripAdvisor";
-  const iosLink = `tripadvisor://${value}`;
-  const androidLink = `intent://${value}#Intent;package=com.tripadvisor.tripadvisor;scheme=https;end`;
+  const iosLink = `tripadvisor://www.tripadvisor.ca/Restaurant_Review-${value}`;
+  const androidLink = `intent://www.tripadvisor.ca/Restaurant_Review-${value}#Intent;package=com.tripadvisor.tripadvisor;scheme=https;end`;
   const href = selectHref(os, iosLink, androidLink);
   return { href, name };
 }
