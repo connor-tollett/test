d1ccc6bd4e3853f89fc60e9f12238599b438a99f
Connor <connor.tollett@icloud.com>
2025-09-30 22:10:35 -0400

    added youtube and fixed small typo in app functions

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
   const container = document.body;
 
   const apps = {
+    youtube: handleYouTube,
     yelp: handleYelp,
     tiktok: handleTikTok,
     instagram: handleInstagram,
--- js/tripadvisor.js
+++ js/tripadvisor.js
@@ -1,7 +1,7 @@
 function handleTripAdvisor(value, os) {
   const name = "TripAdvisor";
   const iosLink = `tripadvisor://${value}`;
-  const androidLink = `intent://${value}/#Intent;package=com.tripadvisor.tripadvisor;scheme=https;end`;
+  const androidLink = `intent://${value}#Intent;package=com.tripadvisor.tripadvisor;scheme=https;end`;
   const href = selectHref(os, iosLink, androidLink);
   return { href, name };
 }
