b74ccbb7cc12ea34d8d319c6ae4806f7b40e8443
Connor <connor.tollett@icloud.com>
2025-10-01 22:45:12 -0400

    removed food ordering

--- js/doordash.js
+++
@@ -1,7 +0,0 @@
-function handleDoorDash(value, os) {
-  const name = "DoorDash";
-  const iosLink = `doordash://store/${value}`;
-  const androidLink = `intent://store/${value}#Intent;package=com.dd.doordash;scheme=doordash;end`;
-  const href = selectHref(os, iosLink, androidLink);
-  return { href, name };
-}
--- js/ubereats.js
+++
@@ -1,7 +0,0 @@
-function handleUberEats(value, os) {
-  const name = "UberEats";
-  const iosLink = `ubereats://store/browse?client_id=eats&storeUUID=${value}`;
-  const androidLink = `ubereats://store/browse?client_id=eats&storeUUID=${value}`;
-  const href = selectHref(os, iosLink, androidLink);
-  return { href, name };
-}
