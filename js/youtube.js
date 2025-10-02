cb593d699877ec608627bc6ae81c46687c6d8d62
Connor <connor.tollett@icloud.com>
2025-10-02 16:54:21 -0400

    modify youtube

--- js/youtube.js
+++ js/youtube.js
@@ -1,7 +1,7 @@
 function handleYouTube(value, os) {
   const name = "YouTube";
   const iosLink = `vnd.youtube://www.youtube.com/@${value}`;
-  const androidLink = `youtube://www.youtube.com/@${value}`;
+  const androidLink = `vnd.youtube://www.youtube.com/@${value}`;
   const href = selectHref(os, iosLink, androidLink);
   return { href, name };
 }
