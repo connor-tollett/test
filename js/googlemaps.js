3209fe31f050495738f990a354eadc32d53b2c54
Connor <connor.tollett@icloud.com>
2025-10-03 14:42:03 -0400

    MODIFY

--- js/googlemaps.js
+++ js/googlemaps.js
@@ -1,7 +1,7 @@
 function handleGoogleMaps(value, os) {
   const name = "GoogleMaps";
-  const iosLink = `comgooglemapsurl://https://www.google.com/maps/place/data=${value}!16s`
-  const androidLink = `intent://www.google.com/maps/place/data=${value}!16s#Intent;package=com.google.android.apps.maps;scheme=https;end`;
+  const iosLink = `comgooglemapsurl://www.google.com/maps/place/data=!4m6!3m5!${value}!16s`
+  const androidLink = `intent://www.google.com/maps/place/data=!4m6!3m5!${value}!16s#Intent;package=com.google.android.apps.maps;scheme=https;end`;
   const href = selectHref(os, iosLink, androidLink);
   return { href, name };
 }
