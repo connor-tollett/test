98b8bfd5fcc46be8ae6983ac94e2a4fc0bd79e1d
Connor <connor.tollett@icloud.com>
2025-10-03 22:45:32 -0400

    modify

--- js/opentable.js
+++ js/opentable.js
@@ -1,6 +1,6 @@
 function handleOpentable(value, os) {
   const name = "Opentable";
-  const iosLink = `opentable://www.opentable.com/${value}/`
+  const iosLink = `opentable://restref?rid=${value}`
   const androidLink = `intent://www.opentable.com/${value}/#Intent;package=com.opentable;scheme=http;end`;
   const href = selectHref(os, iosLink, androidLink);
   return { href, name };
